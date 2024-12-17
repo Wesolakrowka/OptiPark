d243462908ccf40c60c745f81e6366c9ad7b3df3
Martyna Dawidowicz <ul0263726@edu.uni.lodz.pl>
2024-12-17 22:09:25 +0100

    Added frontend registration and login

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+
+// Middleware CORS
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
